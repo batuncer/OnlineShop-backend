512ef1d0c8b0fa5f615aa918063d9b8450cceeeb
Alirezabg <alirezabg@gmail.com>
2025-10-14 11:52:56 +0100

    test

--- src/test/java/com/onlineShop/bootcamp/service/shippingCost/ShippingCostServiceImpTest.java
+++ src/test/java/com/onlineShop/bootcamp/service/shippingCost/ShippingCostServiceImpTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ShippingCostServiceImpTest {
-
+// write test
     private final ShippingCostServiceImp shippingCostService = new ShippingCostServiceImp();
 
     @Test
